8339b505d82c6a3b9e5b7216c0264ad4bd951a96
Lucas Novaes Dragojevic <lucas.dragojevic@gmail.com>
2021-04-06 17:24:28 -0300

    Criada classe que testa serialização com Xstream

---
+++ src/br/com/xstream/Produto.java
@@ -0,0 +1,16 @@
+package br.com.xstream;
+
+public class Produto {
+	
+	private String nome;
+	private Double preco;
+	private String descricao;
+	private Integer codigo;
+	
+	public Produto(String nome, Double preco, String descricao, Integer codigo) {
+		this.nome = nome;
+		this.preco = preco;
+		this.descricao = descricao;
+		this.codigo = codigo;
+	}
+}
---
+++ test/br/com/xstream/ProdutoTest.java
@@ -0,0 +1,29 @@
+package br.com.xstream;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+import com.thoughtworks.xstream.XStream;
+
+public class ProdutoTest {
+	
+	@Test
+	public void deveGerarXMLAdequadamente() {
+		String xmlEsperado = "<produto codigo=\"1587\">\n"
+							+ "  <nome>Geladeira</nome>\n"
+							+ "  <preco>2000.0</preco>\n"
+							+ "  <descrição>Geladeira com freezer invertido</descrição>\n"
+							+ "</produto>";
+		
+		Produto geladeira = new Produto("Geladeira", 2000.00, "Geladeira com freezer invertido", 1587);
+		
+		XStream xstream = new XStream();
+		xstream.alias("produto", Produto.class);
+		xstream.aliasField("descrição", Produto.class, "descricao");
+		xstream.useAttributeFor(Produto.class, "codigo");
+		String xmlGerado = xstream.toXML(geladeira);
+		
+		assertEquals(xmlEsperado, xmlGerado);
+	}
+}
